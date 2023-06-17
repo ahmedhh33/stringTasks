28b77e60b95d0ec5419ba102c2314af1fc9f7cb5
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-17 08:50:21 +0400

    countvowels

--- Program.cs
+++ Program.cs
@@ -4,38 +4,45 @@
     {
         static void Main(string[] args)
         {
-            string input = "Hello, World!";
+            string input = "Hello World!";
             string input1 = "ahmed";
-            string reversed = ReversString(input);
-            Console.WriteLine(reversed);
-            int nofVowels = CountVowels(input1);
-            Console.WriteLine("number of Vowels "+ nofVowels);
+            string reversed = ReversString(input);// EX1
+            Console.WriteLine("The revesed chars in words is  "+reversed);
+            Console.WriteLine("****************************************");
+            int nofVowels = CountVowels(input1);//EX2
+            Console.WriteLine("number of Vowels is " + nofVowels);
+            Console.WriteLine("****************************************");
+
+
+            string reversedword = Reverse(input);//EX3
+            Console.WriteLine(" the reversed word sentence is "+reversedword);
+            Console.WriteLine("****************************************");
 
         }
 
 
 
-        static int CountVowels(string input)
+        static int CountVowels(string input)//EX2 COUNTING vowels
         {
             char[] charArray = input.ToCharArray();
             char[] Vowels = { 'a', 'e', 'i', 'o', 'u' };
             int count = 0;
             for (int i = 0; i < charArray.Length; i++)
             {
-                for (int j = 0;j<Vowels.Length; j++)
+                for (int j = 0; j < Vowels.Length; j++)
                 {
-                    if(charArray[i] ==Vowels[ j])
+                    if (charArray[i] == Vowels[j])
                         count++;
                 }
             }
             return count;
         }
 
-        static string ReversString(string input)
+        static string ReversString(string input)// EX1 REVERS CHARS OF WORDS
         {
             char[] charArray = input.ToCharArray();
             int start = 0;
-            int end = charArray.Length-1;
+            int end = charArray.Length - 1;
             while (start < end)
             {
                 char c = charArray[start];// swap characters
@@ -45,6 +52,28 @@
                 end--;
             }
             return new string(charArray);
+        }
+
+        public static string Reverse(string sentence)//EX3 REVERSE WPRDS IN SENTENCE
+        {
+            // Split the sentence into an array of words.
+            string[] wordsArray = sentence.Split(' ');
+
+            int start = 0;
+            int end = wordsArray.Length - 1;
+            while (start < end)
+            {
+                string c = wordsArray[start];// swap characters
+                wordsArray[start] = wordsArray[end];
+                wordsArray[end] = c;
+                start++;// move indicates towards the center
+                end--;
+            }
+
+            //Join the words back together into a string.
+            string reversedSentence = string.Join(" ", wordsArray);
+            return reversedSentence;
+
         }
     }
 }
